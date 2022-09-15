60cb73633a48320fcd9821e035e92d5839394f2b
Ali Nategh <a.nategh76@gmail.com>
2022-09-15 14:13:49 +0300

    - added Basket.API microservice

---
+++ src/Services/Basket/Basket.API/Models/Entities/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Models.Entities
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+    }
+}
